33c736d70a2375d77e03c7b0f8e028b6a5e0b7b8
Cruelasss <irula5198@gmail.com>
2025-10-24 01:33:10 +0700

    Tampilkan gambar (Image) di UI

--- app/src/main/java/com/example/questadvancelayout_249/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_249/UIPertama.kt
@@ -137,3 +137,10 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
         ) {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 val gambar = painterResource(id = R.drawable.umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
+                )
